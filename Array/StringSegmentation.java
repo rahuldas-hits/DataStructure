4e470fe1889ccf8a7d179ddf93beb31f19481337
Rahul Das <rahuldas.das2012@gmail.com>
2021-06-13 14:53:20 +0530

    String segemnetation

---
+++ Array/StringSegmentation.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class StringSegmentation {
+
+    
+
+    public static void main(String[] args) {
+        String temp_dictionary[] = {"mobile","samsung","sam","sung",
+                            "man","mango","icecream","and",
+                            "go","i","like","ice","cream"};
+
+        for (int i = 0; i < temp_dictionary.length; i++) {
+            System.out.println(temp_dictionary[i]);
+        }
+    }
+}
